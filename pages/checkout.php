2d1af9ef80832d9a6c17ccb17ccc6e0109c2d0d5
TheGatesDev <mtkarels@outlook.com>
2023-05-26 11:07:59 +0100

    Basic checkout layout

--- pages/checkout.php
+++ pages/checkout.php
@@ -14,6 +14,61 @@ $page_js = array("checkout/checkout", "checkout/numpad");
 include_once("../parts/open.php");
 ?>
 <body>
-    
+    <div id="container">
+        <div id="calculator">
+            <input type="text" id="calc-input" autofocus></input>
+            <table id="keypad">
+                <tr>
+                    <td class="keybutton">1</td>
+                    <td class="keybutton">2</td>
+                    <td class="keybutton">3</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="keybutton">4</td>
+                    <td class="keybutton">5</td>
+                    <td class="keybutton">6</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="keybutton">7</td>
+                    <td class="keybutton">8</td>
+                    <td class="keybutton">9</td>
+                    <td class="keybutton">0</td>
+                </tr>
+            </table>
+        </div>
+        <div id="products">
+            <div id="product-display">
+                <h3>Producten</h3>
+                <div id="table-container">
+                    <table id="product-table">
+                        <tr>
+                            <th>Naam</th>
+                            <th>Eenheid</th>
+                            <th>Prijs</th>
+                            <th>Aantal</th>
+                        </tr>
+                        <tr>
+                            <td>TestNaam</td>
+                            <td>TestEenheid</td>
+                            <td>TestPrijs</td>
+                            <td>TestAantal</td>
+                        </tr>
+                    </table>
+                </div>
+                <h3 id="total-price">TotaalPrijs</h3>
+            </div>
+            <div id="product-actions">
+                <div>
+                    <input type="button" class="button-action" id="addbutton" value="+">
+                    <input type="button" class="button-action" id="removebutton" value="-">
+                </div>
+                <div>
+                    <input type="button" class="button-action" id="nextbutton" value="Afrekenen">
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
