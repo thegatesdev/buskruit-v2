ce3cf4c3d5f2d6d20a09a5df5abf7c45a5a2b975
TheGatesDev <mtkarels@outlook.com>
2023-05-26 10:28:16 +0100

    Better relative paths

--- parts/open.php
+++ parts/open.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php
+        $relative_path = $relative_path ?? ".";
         $default_css = array("main", "theme");
         // Set page title
         echo "<title>" . ($page_title ?? "No title") . "</title>";
@@ -13,11 +14,11 @@
         if (isset($page_css)) $page_css = array_merge($page_css, $default_css);
         else $page_css = $default_css;
         foreach ($page_css as $css_name){
-            echo "<link type='text/css' rel='stylesheet' href='./css/$css_name.css'>";
+            echo "<link type='text/css' rel='stylesheet' href='$relative_path/css/$css_name.css'>";
         }
 
         if (isset($page_js)) foreach($page_js as $js_name){
-            echo "<script defer async src='./js/$js_name.js'></script>";
+            echo "<script defer async src='$relative_path/js/$js_name.js'></script>";
         }
     ?>
 </head>
